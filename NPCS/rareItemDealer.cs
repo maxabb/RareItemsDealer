31bffe2740f7a3b91454331afd319abce13ab4bd
unknown <literallyjustm@protonmail.com>
2020-04-11 22:22:54 +1200

    Added Dialog Lines for "rareItemDealer"

--- NPCS/rareItemDealer.cs
+++ NPCS/rareItemDealer.cs
@@ -49,7 +49,31 @@ namespace RareItemsDealer.NPCS {
         }
 
         public override string TownNPCName() {
-            return "qwertyuiop"; // possible names for npc
+            // Pick Random Name
+            switch(Main.rand.Next(10)) {
+                case 0:
+                    return "Casey";
+                case 1:
+                    return "Alexis";
+                case 2:
+                    return "Phoenix";
+                case 3:
+                    return "Billie";
+                case 4:
+                    return "Riley";
+                case 5:
+                    return "Jordan";
+                case 6:
+                    return "Harley";
+                case 7:
+                    return "Ellis";
+                case 8:
+                    return "Elliot";
+                case 9:
+                    return "Taylor";
+                default:
+                    return "Russel";
+            }
         }
 
         public override void NPCLoot() {
@@ -57,9 +81,25 @@ namespace RareItemsDealer.NPCS {
         }
 
         public override string GetChat() {
-            switch(Main.rand.Next(2)) { // get a random value from 0-1
+            // Dialog to only use if there is a tax collector
+            int taxCollector = NPC.FindFirstNPC(NPCID.TaxCollector);
+            if (taxCollector >= 0 && Main.rand.NextBool(7)) {
+                return "Don't tell " + Main.npc[taxCollector].GivenName + " I live here.";
+            }
+
+            switch(Main.rand.Next(7)) { // get a random value
                 case 0: // if it is 0
                     return "Don't ask where I got these."; // say this
+                case 1:
+                    return "If you ever find yourself in the middle of a Columbian maximum security prison, don't trust Steve. You'll find yourself in the shower surrounded by Micky Mouse cosplayers, covered in blood and coughing up cement.";
+                case 2:
+                    return "No, I don't sell vapes. Stop asking that";
+                case 3:
+                    return "Thanks for letting me move in here. I'm pretty sure I'm wanted in every other country";
+                case 4:
+                    return "I'd be selling other stuff as well, but this game's only PG-13";
+                case 5:
+                    return "I also have a website, but you have to be pretty tech savvy to get there";
                 default: { // if it isnt any of the above cases
                     return "Oh you're from Columbia? I stayed at a prison there once."; // say this
                 }
